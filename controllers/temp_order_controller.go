fd670ca408ceb28c1d63871dc906d0211fb6f729
andywijaya15 <andykevonata@gmail.com>
2025-01-02 22:55:33 +0700

    add delete purchase order temp

--- controllers/temp_order_controller.go
+++ controllers/temp_order_controller.go
@@ -2,10 +2,12 @@ package controllers
 
 import (
 	"bima-go/models"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func GetChangedPurchaseOrders(c *gin.Context) {
@@ -29,4 +31,55 @@ func GetChangedPurchaseOrders(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, tempOrders)
+}
+
+func deleteOrder(tx *gorm.DB, tableName, cOrderId string) error {
+	err := tx.Table(tableName).
+		Where("issotrx = ?", "N").
+		Where("c_order_id = ?", cOrderId).
+		Delete(nil).Error
+
+	if err != nil {
+		return fmt.Errorf("failed to delete from %s: %w", tableName, err)
+	}
+	return nil
+}
+
+func DeletePurchaseOrder(c *gin.Context) {
+	cOrderId := c.Query("c_order_id")
+	if cOrderId == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "c_order_id is required"})
+		return
+	}
+	tx := models.DB.Begin()
+	if tx.Error != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to start transaction", "details": tx.Error.Error()})
+		return
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+			panic(r)
+		} else if tx.Error != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
+		}
+	}()
+	if err := tx.Table("bima_order_temp").
+		Where("issotrx = ?", "N").
+		Where("c_order_id = ?", cOrderId).
+		Delete(nil).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := tx.Table("bima_orderline_temp").
+		Where("c_order_id = ?", cOrderId).
+		Delete(nil).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Purchase order deleted successfully"})
 }
