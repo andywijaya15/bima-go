b5f5e7ec790ad18fa866f005d622522a5ac25d4c
andywijaya15 <andykevonata@gmail.com>
2025-01-02 22:27:24 +0700

    add threshold when query temp order

--- controllers/temp_order_controller.go
+++ controllers/temp_order_controller.go
@@ -3,24 +3,29 @@ package controllers
 import (
 	"bima-go/models"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetChangedPurchaseOrders(c *gin.Context) {
 	var tempOrders []models.TempOrder
+	threshold := time.Now()
+
+	// Subtract 3 months from the threshold
+	threshold = threshold.AddDate(0, -3, 0)
 	bimaOrderTempQuery := models.DB.Table("bima_order_temp").
 		Select("c_order_id, DATE(created) AS created").
 		Where("issotrx = ?", "N").
-		Where("created::date >= ?", "2024-01-01")
+		Where("created::date >= ?", threshold)
 
 	bimaOrderLineTempQuery := models.DB.Table("bima_orderline_temp").
 		Select("c_order_id, DATE(created) AS created").
-		Where("created::date >= ?", "2024-01-01")
+		Where("created::date >= ?", threshold)
 
 	err := models.DB.Raw("SELECT DISTINCT * FROM (? UNION ALL ?) tbl", bimaOrderTempQuery, bimaOrderLineTempQuery).Scan(&tempOrders).Error
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
 		return
 	}
 	c.JSON(http.StatusOK, tempOrders)
