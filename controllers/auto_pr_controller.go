1a7e4e41e0c0684c1a1cd14c57b1df91bc55db76
andywijaya15 <andykevonata@gmail.com>
2025-01-09 13:04:10 +0700

    add autopr

--- controllers/auto_pr_controller.go
+++ controllers/auto_pr_controller.go
@@ -8,26 +8,69 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetAutoPr(c *gin.Context) {
-	var orderDetails []models.OrderDetail
-	lcDate := time.Now().AddDate(0, -3, 0)
-	lastUpdatePo := time.Now().AddDate(0, -4, 0)
+type OrderDetail struct {
+	TableName              string
+	ID                     int
+	COrderlineID           int
+	COrderID               int
+	CBPartnerID            int
+	ItemID                 int
+	FactoryID              int
+	SoID                   int
+	CategoryID             int
+	PurchaseNumber         string
+	SupplierName           string
+	Category               string
+	ItemCode               string
+	UOM                    string
+	QtyAllocation          float64
+	POBuyer                string
+	LCDate                 string
+	IsRecycle              string
+	StatusLC               string
+	StdPrecision           int
+	SoOrderType            string
+	Season                 string
+	WarehousePlace         string
+	SoDocTypeID            int
+	PromiseDate            time.Time
+	JobOrder               string
+	PurchaseDocumentTypeID int
+	IsFabric               string
+	Color                  string
+	UOMID                  int
+	ItemDesc               string
+	LastUpdatePO           time.Time
+	IsMrpExists            bool
+}
 
-	err := models.DB.Table("bw_wms_auto_pr as bwap").
+var DB = models.DB
+
+func GetAutoPr(c *gin.Context) {
+	lcDate := time.Now().AddDate(0, -2, 0)
+	lastUpdatePo := time.Now().AddDate(0, -2, 0)
+
+	rows, err := models.DB.Table("bw_wms_auto_pr as bwap").
 		Select("*").
-		// Where("bwap.lc_date >= ?", lcDate).
-		// Where("bwap.last_update_po >= ?", lastUpdatePo).
+		Where("bwap.lc_date >= ?", lcDate).
+		Where("bwap.last_update_po >= ?", lastUpdatePo).
 		Where("bwap.is_mrp_exists = ?", true).
-		Where(models.OrderDetail{
-			LCDate:       lcDate,
-			LastUpdatePO: lastUpdatePo,
-		}).
-		Limit(1).
-		Scan(&orderDetails).Error
+		Rows()
 
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
 		return
 	}
+	defer rows.Close()
+	var orderDetails []OrderDetail
+	for rows.Next() {
+		var orderDetail OrderDetail
+		models.DB.ScanRows(rows, &orderDetail)
+		orderDetails = append(orderDetails, orderDetail)
+		if len(orderDetails) >= 100 {
+			break
+		}
+	}
+
 	c.JSON(http.StatusOK, orderDetails)
 }
